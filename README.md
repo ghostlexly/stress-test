8852dcc869273a7893a93042b8c0a005ce0da2a8
ghostlexly <ghostlexly@gmail.com>
2026-06-12 10:30:04 +0200

    Add initial setup for `stress-test` project
    Includes:
    - Basic `README.md` with installation and usage instructions
    - `index.ts` defining a K6 stress test scenario
    - Configuration files: `bun.lock`, `package.json`, and `tsconfig.json`

---
+++ README.md
@@ -0,0 +1,13 @@
+# stress-test
+
+To install k6 on MacOS
+
+```bash
+brew install k6
+```
+
+To launch the script
+
+```bash
+k6 run index.ts
+```
---
+++ index.ts
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+
+export const options = {
+  scenarios: {
+    constant_load: {
+      executor: 'constant-arrival-rate',
+      rate: 50,              // 50 req/s, indépendant des réponses
+      timeUnit: '1s',
+      duration: '2m',
+      preAllocatedVUs: 100,  // marge si ça ralentit
+      maxVUs: 500,
+    },
+  },
+  thresholds: {
+    http_req_duration: ['p(95)<300', 'p(99)<800', 'p(99.9)<2000'],
+  },
+};
+
+export default function () {
+  http.get('https://ton-domaine/api/search?q=test');
+}
